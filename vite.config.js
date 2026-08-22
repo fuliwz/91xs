dfad21f95014ae24146c71afb0421d5d0a43ed9e
fuliwz <92189180+fuliwz@users.noreply.github.com>
2026-08-22 21:33:33 +0800

    fix: proxy video API through lbapi9 in development

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-export default defineConfig({ plugins:[vue()], server:{ proxy:{ '/api':{ target:'https://155api.com', changeOrigin:true, rewrite:p=>p.replace(/^\/api/,'/api.php/provide/vod') }}}})
+
+export default defineConfig({
+  plugins: [vue()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://lbapi9.com',
+        changeOrigin: true,
+        rewrite: p => p.replace(/^\/api\/?/, '/api.php/provide/vod'),
+      },
+    },
+  },
+})

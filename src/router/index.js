dd582a7faf6a1d673e7bc02ab5f3c660bbeaf910
fuliwz <92189180+fuliwz@users.noreply.github.com>
2026-08-22 22:39:38 +0800

    feat: centralize route paths and page titles

--- src/router/index.js
+++ src/router/index.js
@@ -8,16 +8,17 @@ import Image from '../views/Image.vue'
 import ImageDetail from '../views/ImageDetail.vue'
 import Novel from '../views/Novel.vue'
 import NovelDetail from '../views/NovelDetail.vue'
+import site from '../config/site'
 
 export default [
-  { path: '/', component: Home },
-  { path: '/category/:id', component: Category },
-  { path: '/search', component: Search },
-  { path: '/detail/:id', component: Detail },
-  { path: '/play/:id', component: Play },
-  { path: '/history', component: History },
-  { path: '/image', component: Image },
-  { path: '/image/:id', component: ImageDetail },
-  { path: '/novel', component: Novel },
-  { path: '/novel/:id', component: NovelDetail },
+  { path: site.routes.home, component: Home, meta: { titleKey: 'home' } },
+  { path: site.routes.category, component: Category, meta: { titleKey: 'category' } },
+  { path: site.routes.search, component: Search, meta: { titleKey: 'search' } },
+  { path: site.routes.detail, component: Detail, meta: { titleKey: 'detail' } },
+  { path: site.routes.play, component: Play, meta: { titleKey: 'play' } },
+  { path: site.routes.history, component: History, meta: { titleKey: 'history' } },
+  { path: site.routes.image, component: Image, meta: { titleKey: 'image' } },
+  { path: site.routes.imageDetail, component: ImageDetail, meta: { titleKey: 'imageDetail' } },
+  { path: site.routes.novel, component: Novel, meta: { titleKey: 'novel' } },
+  { path: site.routes.novelDetail, component: NovelDetail, meta: { titleKey: 'novelDetail' } },
 ]

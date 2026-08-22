a0c26d6c2582db6f1a4fd93099bb1d011c126492
fuliwz <92189180+fuliwz@users.noreply.github.com>
2026-08-22 15:07:00 +0800

    feat: add article card component

---
+++ src/components/ArticleCard.vue
@@ -0,0 +1,17 @@
+<template>
+  <router-link class="article-card" :to="to">
+    <div class="article-thumb">
+      <img v-if="pic" :src="pic" :alt="title" loading="lazy" />
+      <span v-else>文章</span>
+    </div>
+    <div class="article-card-title">{{ title }}</div>
+  </router-link>
+</template>
+
+<script setup>
+defineProps({
+  title: { type: String, default: '未命名文章' },
+  pic: { type: String, default: '' },
+  to: { type: String, default: '/image' },
+})
+</script>

f08abf69496d95758da849876cf58dad8e810e6f
fuliwz <92189180+fuliwz@users.noreply.github.com>
2026-08-22 22:50:26 +0800

    refactor: centralize footer site content

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,11 @@
 <template>
   <footer>
-    <div class="footer-contact">联系邮箱：service@91xs.example</div>
-    <div class="foot footer-links">友情链接</div>
-    <div class="disclaimer">内容与数据由接口服务提供。请遵守当地法律法规并合理使用网络内容，本页面仅提供前端展示与索引功能。</div>
+    <div class="footer-contact">{{ site.footer.email }}</div>
+    <div class="foot footer-links">{{ site.footer.linksTitle }}</div>
+    <div class="disclaimer">{{ site.footer.disclaimer }}</div>
   </footer>
 </template>
+
+<script setup>
+import site from '../config/site'
+</script>

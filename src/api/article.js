fa1ff2fc8340491e61678455e7e2f55faf759b18
fuliwz <92189180+fuliwz@users.noreply.github.com>
2026-08-22 15:25:29 +0800

    增加文章分类与按分类查询API

--- src/api/article.js
+++ src/api/article.js
@@ -1,6 +1,8 @@
 import { request, ART_API_BASE } from './client'
 
+export const getArticleCategories = () => request({ ac: 'list' }, ART_API_BASE)
 export const getArticles = (params = {}) => request({ ac: 'list', pg: 1, limit: 20, ...params }, ART_API_BASE)
+export const getArticlesByCategory = (tid, page = 1, limit = 20) => request({ ac: 'list', t: tid, pg: page, limit }, ART_API_BASE)
 export const getArticleDetail = id => request({ ac: 'detail', ids: id }, ART_API_BASE)
 export const searchArticles = (wd, page = 1) => request({ ac: 'list', wd, pg: page, limit: 20 }, ART_API_BASE)
 
@@ -22,4 +24,8 @@ export function articlePic(item) {
 
 export function articleContent(item) {
   return item?.art_content || item?.content || item?.art_blurb || item?.description || ''
+}
+
+export function articleCategoryId(item) {
+  return Number(item?.type_id || item?.art_type_id || item?.category_id || 0)
 }

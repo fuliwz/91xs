57835d73a151c3b5829d3d589a0c94e0818d5e47
fuliwz <92189180+fuliwz@users.noreply.github.com>
2026-08-22 15:07:29 +0800

    feat: add image and novel routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,2 +1,23 @@
-import Home from '../views/Home.vue'; import Category from '../views/Category.vue'; import Search from '../views/Search.vue'; import Detail from '../views/Detail.vue'; import Play from '../views/Play.vue'; import History from '../views/History.vue'
-export default [{path:'/',component:Home},{path:'/category/:id',component:Category},{path:'/search',component:Search},{path:'/detail/:id',component:Detail},{path:'/play/:id',component:Play},{path:'/history',component:History}]
+import Home from '../views/Home.vue'
+import Category from '../views/Category.vue'
+import Search from '../views/Search.vue'
+import Detail from '../views/Detail.vue'
+import Play from '../views/Play.vue'
+import History from '../views/History.vue'
+import Image from '../views/Image.vue'
+import ImageDetail from '../views/ImageDetail.vue'
+import Novel from '../views/Novel.vue'
+import NovelDetail from '../views/NovelDetail.vue'
+
+export default [
+  { path: '/', component: Home },
+  { path: '/category/:id', component: Category },
+  { path: '/search', component: Search },
+  { path: '/detail/:id', component: Detail },
+  { path: '/play/:id', component: Play },
+  { path: '/history', component: History },
+  { path: '/image', component: Image },
+  { path: '/image/:id', component: ImageDetail },
+  { path: '/novel', component: Novel },
+  { path: '/novel/:id', component: NovelDetail },
+]

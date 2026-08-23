7326cfe99f0f26c3a47c1bc460f4bb74dfc5e4f5
fuliwz <92189180+fuliwz@users.noreply.github.com>
2026-08-23 12:25:35 +0800

    feat: integrate advertisements into app shell

--- src/App.vue
+++ src/App.vue
@@ -1,2 +1,12 @@
-<template><Header/><main class="page"><router-view/></main><Footer/></template>
-<script setup>import Header from './components/Header.vue';import Footer from './components/Footer.vue'</script>
+<template>
+  <Header />
+  <AdContainer />
+  <main class="page"><router-view /></main>
+  <Footer />
+</template>
+
+<script setup>
+import Header from './components/Header.vue'
+import AdContainer from './components/AdContainer.vue'
+import Footer from './components/Footer.vue'
+</script>

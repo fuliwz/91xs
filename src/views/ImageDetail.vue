227a69ce43cf1ba748f113c5a39508ff82b18f21
fuliwz <92189180+fuliwz@users.noreply.github.com>
2026-08-23 15:57:35 +0800

    security: sanitize article html

--- src/views/ImageDetail.vue
+++ src/views/ImageDetail.vue
@@ -8,13 +8,16 @@
 
 <script setup>
 import { computed, onMounted, ref } from 'vue'
+import DOMPurify from 'dompurify'
 import { useRoute } from 'vue-router'
 import { getArticleDetail, articleItem, articleTitle, articleContent } from '../api/article'
 
 const route = useRoute()
 const item = ref(null)
 const title = computed(() => item.value ? articleTitle(item.value) : '图片详情')
-const content = computed(() => articleContent(item.value))
+const content = computed(() => DOMPurify.sanitize(articleContent(item.value), {
+  USE_PROFILES: { html: true },
+}))
 
 onMounted(async () => {
   try { item.value = articleItem(await getArticleDetail(route.params.id)) } catch { item.value = null }

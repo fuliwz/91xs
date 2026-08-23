e9a704242fe5e28b9ed4e238b96eb81fd40c7d67
fuliwz <92189180+fuliwz@users.noreply.github.com>
2026-08-23 15:39:10 +0800

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,6 @@
 <template>
   <footer>
-    <div class="footer-contact">{{ site.footer.email }}</div>
-    <div class="foot footer-links">{{ site.footer.linksTitle }}</div>
+
     <div class="disclaimer">{{ site.footer.disclaimer }}</div>
   </footer>
 </template>

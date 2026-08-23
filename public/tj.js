c59362a74adfee2dccb34b5591a8864051a8bd3b
fuliwz <92189180+fuliwz@users.noreply.github.com>
2026-08-23 12:35:25 +0800

    refactor: move Histats tracking to tj.js

---
+++ public/tj.js
@@ -0,0 +1,30 @@
+(function () {
+  window._Hasync = window._Hasync || [];
+
+  window._Hasync.push([
+    'Histats.start',
+    '1,4670594,4,0,0,0,00010000'
+  ]);
+
+  window._Hasync.push([
+    'Histats.fasi',
+    '1'
+  ]);
+
+  window._Hasync.push([
+    'Histats.track_hits',
+    ''
+  ]);
+
+  if (document.querySelector('script[data-histats="1"]')) {
+    return;
+  }
+
+  var hs = document.createElement('script');
+  hs.type = 'text/javascript';
+  hs.async = true;
+  hs.dataset.histats = '1';
+  hs.src = 'https://s10.histats.com/js15_as.js';
+
+  (document.head || document.body).appendChild(hs);
+})();

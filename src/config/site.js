38287715da086c1c7c74463b3020927854adbdec
fuliwz <92189180+fuliwz@users.noreply.github.com>
2026-08-23 15:46:35 +0800

    fix: escape multiline footer disclaimer

--- src/config/site.js
+++ src/config/site.js
@@ -18,11 +18,10 @@ const site = {
   },
 
   footer: {
-
-    disclaimer: '警告︰本網站只這合十八歲或以上人士觀看。內容可能令人反感；不可將本網站的內容派發、傳閱、出售、出租、交給或借予年齡未滿18歲的人士或將本網站內容向該人士出示、播放或放映。
+    disclaimer: `警告︰本網站只這合十八歲或以上人士觀看。內容可能令人反感；不可將本網站的內容派發、傳閱、出售、出租、交給或借予年齡未滿18歲的人士或將本網站內容向該人士出示、播放或放映。
 LEGAL DISCLAIMER WARNING: THIS FORUM CONTAINS MATERIAL WHICH MAY OFFEND AND MAY NOT BE DISTRIBUTED, CIRCULATED, SOLD, HIRED, GIVEN, LENT,SHOWN, PLAYED OR PROJECTED TO A PERSON UNDER THE AGE OF 18 YEARS.
 
-站点申明：我们立足于美利坚合众国，受北美法律保护,未满18岁或被误导来到这里，请立即离开',
+站点申明：我们立足于美利坚合众国，受北美法律保护,未满18岁或被误导来到这里，请立即离开`,
   },
 
   routes: {
